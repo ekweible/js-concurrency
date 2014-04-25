3e8532bcf8a365cc87cd1454e480cd32fc86a2b1
Evan.Weible <ekweible@gmail.com>
2014-04-25 06:19:15 -0500

    Add transferables example

---
+++ examples/transferables/cloneWorker.js
@@ -0,0 +1,9 @@
+self.addEventListener('message', function(evt) {
+    var largeObject = evt.data.obj;
+    var payload = {
+        obj: largeObject,
+        duration: Date.now() - evt.data.time,
+        time: Date.now()
+    };
+    self.postMessage(payload);
+});
